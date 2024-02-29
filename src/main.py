b8304e7e195d12927837eea7703f60442572238d
ZhenyaKasatkina <knopisha.zh@gmail.com>
2024-02-29 19:54:35 +0300

    change src/main.py, add src/config.py, src/create_databases.py, src/valid_vacancy.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from src.hh_api import HeadHunterAPI
+from src.config import config
+from src.create_databases import create_database, save_data_to_database
+
 
 companies = ["Планетра", "ЛАНИТ", "Фабрика решений", "InlyIT", "Тензор", "Инспектор Клауд",
              "ВЕБ Инфраструктура", "БИЗНЕС КОНТЕНТ", "Syntella", "ANA BAR", "Wanted",
@@ -9,7 +12,12 @@ companies = ["Планетра", "ЛАНИТ", "Фабрика решений", 
 def main():
     hh_data = HeadHunterAPI(companies)
     ads_data = hh_data.get_vacancies_data()
-    print(ads_data)
+    # print(ads_data)
+
+    params = config()
+
+    create_database("vacancies_with_hh", params)
+    save_data_to_database(ads_data, "vacancies_with_hh", params)
 
 
 if __name__ == '__main__':
